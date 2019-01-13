3dde09716e9e4adcec1c1aabfc89df8287d4abfe
Sheldon Nunes <Sheldon.Nunes@fphcare.co.nz>
2019-01-14 09:30:19 +1300

    change implements assignment

---
+++ interfaces_assignment/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type area interface {
+	printArea()
+}
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base float64
+}
+
+type square struct {
+	sidelength float64
+}
+
+
+
+func (s square) getArea() float64 {
+	return s.sidelength * s.sidelength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) printArea() {
+	fmt.Println(s.getArea())
+}
+
+func (t triangle) printArea() {
+	fmt.Println(t.getArea())
+}
+
+
+func main() {
+}
